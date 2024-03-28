803c6c0c28201143a86a3adbeeecb855c643a561
CarlosBrunodev <carlosbruno.dev@gmail.com>
2024-03-28 14:02:50 -0300

    [Trello-03] Instrumentalizando aplicacao com prometheus client - metrics

--- app/api.py
+++ app/api.py
@@ -1,11 +1,14 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
-
+from flask import Flask, jsonify, request
+from prometheus_client import Counter, generate_latest, REGISTRY, Gauge
+from prometheus_client.exposition import MetricsHandler
 
 app_name = 'comentarios'
 app = Flask(app_name)
 app.debug = True
+
+# Definindo métricas Prometheus
+comments_counter = Counter('comments_created', 'Number of comments created')
+comments_gauge = Gauge('comments_total', 'Total number of comments')
 
 comments = {}
 
@@ -28,6 +31,9 @@ def api_comment_new():
     else:
         comments[content_id] = [new_comment]
 
+    comments_counter.inc()  # Incrementando a métrica de contador
+    comments_gauge.set(len(comments))  # Atualizando a métrica de gauge
+
     message = 'comment created and associated with content_id {}'.format(content_id)
     response = {
             'status': 'SUCCESS',
@@ -49,3 +55,16 @@ def api_comment_list(content_id):
                 'message': message,
                 }
         return jsonify(response), 404
+
+
+# Endpoint para as métricas Prometheus
+@app.route('/metrics')
+def prometheus_metrics():
+    return generate_latest(REGISTRY), 200, {'Content-Type': 'text/plain'}
+
+# Registro do manipulador MetricsHandler para garantir que o Flask possa servir as métricas corretamente
+app.add_url_rule('/metrics', 'metrics', view_func=prometheus_metrics)
+
+
+if __name__ == '__main__':
+    app.run()
