49404a6c733d6a55d61feb5a5ce5013ed7389415
CarlosBrunodev <carlosbruno.dev@gmail.com>
2024-03-30 08:53:44 -0300

    [Trello-07] Criacao de arquivo terraform para criacao do cluster com rke

---
+++ terraform/rke/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+
+  required_providers {
+    rke = {
+      source = "rancher/rke"
+      version = "1.4.4"
+    }
+  }
+}
+
+provider "rke" {
+  debug = false
+}
+
+
+resource "rke_cluster" "foo2" {
+  nodes {
+    address = "13.88.190.115"
+    user    = "adminuser"
+    role    = ["controlplane", "worker", "etcd"]
+    ssh_key = file("~/.ssh/id_rsa")
+  }
+  nodes {
+    address = "13.88.190.114"
+    user    = "adminuser"
+    role    = ["controlplane", "worker", "etcd"]
+    ssh_key = file("~/.ssh/id_rsa")
+  }
+  upgrade_strategy {
+      drain = true
+      max_unavailable_worker = "20%"
+  }
+
+  enable_cri_dockerd = true
+}
---
+++ terraform/rke/outuput.tf
@@ -0,0 +1,5 @@
+output "kubeconfig" {
+  description = "Virtual Machine name IP Address"
+  value       = rke_cluster.foo2.kube_config_yaml
+  sensitive = true
+}
